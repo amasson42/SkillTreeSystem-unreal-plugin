33df0adc44f52754c88d76c8b7e3033437c4c3de
amasson42 <arthur.masson.3896@gmail.com>
2026-04-22 22:34:40 +0200

    Multiple Tree in skill source data asset

--- Source/SkillTreeSystem/Private/SkillTreeSystem/Data/SkillTreeSourceDataAsset.cpp
+++ Source/SkillTreeSystem/Private/SkillTreeSystem/Data/SkillTreeSourceDataAsset.cpp
@@ -7,58 +7,79 @@
 
 void USkillTreeSourceDataAsset::GetNodesIds_Implementation(const FGameplayTag& TreeCategory, TArray<FGameplayTag>& OutNodes)
 {
-	Nodes.GetKeys(OutNodes);
+	if (const auto* Tree = Trees.Find(TreeCategory))
+	{
+		Tree->Nodes.GetKeys(OutNodes);
+	}
 }
 
 void USkillTreeSourceDataAsset::GetNodeClass_Implementation(const FGameplayTag& TreeCategory, const FGameplayTag& NodeId, TSubclassOf<USkillTreeNodeWidget>& OutWidgetClass)
 {
-	OutWidgetClass = NodeWidgetClass;
+	if (const auto* Tree = Trees.Find(TreeCategory))
+	{
+		OutWidgetClass = Tree->NodeWidgetClass;
+	}
 }
 
 void USkillTreeSourceDataAsset::GetNodeAppearance_Implementation(const FGameplayTag& TreeCategory, const FGameplayTag& NodeId, FSkillTreeNodeAppearance& OutAppearance)
 {
-	if (FSkillTreeSourceDataAssetElement* Element = Nodes.Find(NodeId))
+	if (const auto* Tree = Trees.Find(TreeCategory))
 	{
-		OutAppearance = Element->Appearance;
+		if (const auto* Element = Tree->Nodes.Find(NodeId))
+		{
+			OutAppearance = Element->Appearance;
+		}
 	}
 }
 
 void USkillTreeSourceDataAsset::GetNodesPosition_Implementation(const FGameplayTag& TreeCategory, TMap<FGameplayTag, FSkillTreeNodePosition>& NodePositions)
 {
-	if (!(LayoutSource && IsValid(LayoutSource.GetObject())))
-		return;
-
+	const auto* Tree = Trees.Find(TreeCategory);
+	if (!Tree) return;
+	
+	if (!(Tree->LayoutSource && IsValid(Tree->LayoutSource.GetObject()))) return;
+	
 	TMap<FSkillTreeLinkName, FSkillTreeLinkPosition> LinksPositions;
-	ISkillTreeLayoutMaker::Execute_GetElementsPosition(LayoutSource.GetObject(), NodePositions, LinksPositions);
+	ISkillTreeLayoutMaker::Execute_GetElementsPosition(Tree->LayoutSource.GetObject(), NodePositions, LinksPositions);
 }
 
 void USkillTreeSourceDataAsset::GetLinks_Implementation(const FGameplayTag& TreeCategory, TArray<FSkillTreeLinkName>& OutLinks)
 {
-	if (!(LayoutSource && IsValid(LayoutSource.GetObject())))
-		return;
+	const auto* Tree = Trees.Find(TreeCategory);
+	if (!Tree) return;
+	
+	if (!(Tree->LayoutSource && IsValid(Tree->LayoutSource.GetObject()))) return;
 	
 	TMap<FGameplayTag, FSkillTreeNodePosition> NodePositions;
 	TMap<FSkillTreeLinkName, FSkillTreeLinkPosition> LinksPositions;
-	ISkillTreeLayoutMaker::Execute_GetElementsPosition(LayoutSource.GetObject(), NodePositions, LinksPositions);
+	ISkillTreeLayoutMaker::Execute_GetElementsPosition(Tree->LayoutSource.GetObject(), NodePositions, LinksPositions);
 	
 	LinksPositions.GetKeys(OutLinks);
 }
 
 void USkillTreeSourceDataAsset::GetLinkClass_Implementation(const FGameplayTag& TreeCategory, const FSkillTreeLinkName& LinkName, TSubclassOf<USkillTreeLinkWidget>& OutWidgetClass)
 {
-	OutWidgetClass = LinkWidgetClass;
+	if (const auto* Tree = Trees.Find(TreeCategory))
+	{
+		OutWidgetClass = Tree->LinkWidgetClass;
+	}
 }
 
 void USkillTreeSourceDataAsset::GetLinkAppearance_Implementation(const FGameplayTag& TreeCategory, const FSkillTreeLinkName& LinkName, FSkillTreeLinkAppearance& OutAppearance)
 {
-	OutAppearance = LinkAppearance;
+	if (const auto* Tree = Trees.Find(TreeCategory))
+	{
+		OutAppearance = Tree->LinkAppearance;
+	}
 }
 
 void USkillTreeSourceDataAsset::GetLinksPositions_Implementation(const FGameplayTag& TreeCategory, TMap<FSkillTreeLinkName, FSkillTreeLinkPosition>& OutLinksPositions)
 {
-	if (!(LayoutSource && IsValid(LayoutSource.GetObject())))
-		return;
-
+	const auto* Tree = Trees.Find(TreeCategory);
+	if (!Tree) return;
+	
+	if (!(Tree->LayoutSource && IsValid(Tree->LayoutSource.GetObject()))) return;
+	
 	TMap<FGameplayTag, FSkillTreeNodePosition> NodePositions;
-	ISkillTreeLayoutMaker::Execute_GetElementsPosition(LayoutSource.GetObject(), NodePositions, OutLinksPositions);
+	ISkillTreeLayoutMaker::Execute_GetElementsPosition(Tree->LayoutSource.GetObject(), NodePositions, OutLinksPositions);
 }
--- Source/SkillTreeSystem/Public/SkillTreeSystem/Data/SkillTreeSourceDataAsset.h
+++ Source/SkillTreeSystem/Public/SkillTreeSystem/Data/SkillTreeSourceDataAsset.h
@@ -21,25 +21,15 @@ public:
 	
 };
 
-/**
- * Implementation of a mono tree and mono widget type and data driven node inputs
- */
-UCLASS()
-class SKILLTREESYSTEM_API USkillTreeSourceDataAsset : public UDataAsset, public ISkillTreeSourceInterface
+USTRUCT(BlueprintType)
+struct SKILLTREESYSTEM_API FSkillTreeSourceDataAssetTreeCategory
 {
 	GENERATED_BODY()
 	
 public:
-
-	void GetNodesIds_Implementation(const FGameplayTag& TreeCategory, TArray<FGameplayTag>& OutNodes) override;
-	void GetNodeClass_Implementation(const FGameplayTag& TreeCategory, const FGameplayTag& NodeId, TSubclassOf<USkillTreeNodeWidget>& OutWidgetClass) override;
-	void GetNodeAppearance_Implementation(const FGameplayTag& TreeCategory, const FGameplayTag& NodeId, FSkillTreeNodeAppearance& OutAppearance) override;
-	void GetNodesPosition_Implementation(const FGameplayTag& TreeCategory, TMap<FGameplayTag, FSkillTreeNodePosition>& NodePositions) override;
 	
-	void GetLinks_Implementation(const FGameplayTag& TreeCategory, TArray<FSkillTreeLinkName>& OutLinks) override;
-	void GetLinkClass_Implementation(const FGameplayTag& TreeCategory, const FSkillTreeLinkName& LinkName, TSubclassOf<USkillTreeLinkWidget>& OutWidgetClass) override;
-	void GetLinkAppearance_Implementation(const FGameplayTag& TreeCategory, const FSkillTreeLinkName& LinkName, FSkillTreeLinkAppearance& OutAppearance) override;
-	void GetLinksPositions_Implementation(const FGameplayTag& TreeCategory, TMap<FSkillTreeLinkName, FSkillTreeLinkPosition>& OutLinksPositions) override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Content")
+	TMap<FGameplayTag, FSkillTreeSourceDataAssetElement> Nodes;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Widget|Node")
 	TSubclassOf<USkillTreeNodeWidget> NodeWidgetClass;
@@ -50,10 +40,33 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Widget|Link")
 	FSkillTreeLinkAppearance LinkAppearance;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Data")
-	TMap<FGameplayTag, FSkillTreeSourceDataAssetElement> Nodes;
-	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Layout")
 	TScriptInterface<ISkillTreeLayoutMaker> LayoutSource;
+	
+};
+
+
+/**
+ * Implementation of a mono tree and mono widget type and data driven node inputs
+ */
+UCLASS()
+class SKILLTREESYSTEM_API USkillTreeSourceDataAsset : public UDataAsset, public ISkillTreeSourceInterface
+{
+	GENERATED_BODY()
+	
+public:
+	
+	void GetNodesIds_Implementation(const FGameplayTag& TreeCategory, TArray<FGameplayTag>& OutNodes) override;
+	void GetNodeClass_Implementation(const FGameplayTag& TreeCategory, const FGameplayTag& NodeId, TSubclassOf<USkillTreeNodeWidget>& OutWidgetClass) override;
+	void GetNodeAppearance_Implementation(const FGameplayTag& TreeCategory, const FGameplayTag& NodeId, FSkillTreeNodeAppearance& OutAppearance) override;
+	void GetNodesPosition_Implementation(const FGameplayTag& TreeCategory, TMap<FGameplayTag, FSkillTreeNodePosition>& NodePositions) override;
+	
+	void GetLinks_Implementation(const FGameplayTag& TreeCategory, TArray<FSkillTreeLinkName>& OutLinks) override;
+	void GetLinkClass_Implementation(const FGameplayTag& TreeCategory, const FSkillTreeLinkName& LinkName, TSubclassOf<USkillTreeLinkWidget>& OutWidgetClass) override;
+	void GetLinkAppearance_Implementation(const FGameplayTag& TreeCategory, const FSkillTreeLinkName& LinkName, FSkillTreeLinkAppearance& OutAppearance) override;
+	void GetLinksPositions_Implementation(const FGameplayTag& TreeCategory, TMap<FSkillTreeLinkName, FSkillTreeLinkPosition>& OutLinksPositions) override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Data")
+	TMap<FGameplayTag, FSkillTreeSourceDataAssetTreeCategory> Trees;
 	
 };
